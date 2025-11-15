cd704e27745da2faca74d12a0a832ceeccd225ae
temperancemonkey <downt872@newschool.edu>
2025-11-14 22:38:12 -0500

    .

--- script.js
+++ script.js
@@ -38,6 +38,39 @@ workOne.addEventListener('click', () => {
     }
 });
 
+const workTwo = document.getElementById('workTwo');
+const workTwoImg = document.getElementById('workTwoImg');
+
+workTwo.addEventListener('click', () => {
+    if (workTwoImg.style.display === 'block') {
+        workTwoImg.style.display = 'none';
+    } else {
+        workTwoImg.style.display = 'block';
+    }
+});
+
+const workThree = document.getElementById('workThree');
+const workThreeImg = document.getElementById('workThreeImg');
+
+workThree.addEventListener('click', () => {
+    if (workThreeImg.style.display === 'block') {
+        workThreeImg.style.display = 'none';
+    } else {
+        workThreeImg.style.display = 'block';
+    }
+});
+
+const workFour = document.getElementById('workFour');
+const workFourImg = document.getElementById('workFourImg');
+
+workFour.addEventListener('click', () => {
+    if (workFourImg.style.display === 'block') {
+        workFourImg.style.display = 'none';
+    } else {
+        workFourImg.style.display = 'block';
+    }
+});
+
 
 
 
