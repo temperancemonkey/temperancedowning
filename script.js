efc4b503faaf43b9179271d2785a9c28fc458353
temperancemonkey <downt872@newschool.edu>
2026-01-14 15:10:17 -0500

    update website

--- script.js
+++ script.js
@@ -1,141 +1,34 @@
-// INFO
+// TEST
 
-const hoverElement = document.getElementById('aboutHover');
-const popupText = document.getElementById('aboutText');
+const gridItems = document.querySelectorAll('.grid-item');
+gridItems.forEach(item => {
+    item.addEventListener('click', () => {
+        const targetId = item.dataset.target;
+        const content = document.getElementById(targetId);
 
-hoverElement.addEventListener('mouseenter', () => {
-    popupText.style.display = 'block';
+        if (!content) return;
+        
+        document.querySelectorAll('.gridContents').forEach(div => {
+            if(div !== content) div.style.display = 'none';
+        });
+
+        content.style.display = 
+            content.style.display === 'block' ? 'none' : 'block';
+
+    });
 });
 
-hoverElement.addEventListener('mouseleave', () => {
-    popupText.style.display = 'none';
-});
+// const gridItem = document.getElementsByClassName('grid-item');
+// const gridContent = document.getElementsByClassName('gridContents') [0] ;
 
+// gridItem[0].addEventListener('click', () => {
+//     if(gridContent.style.display === 'block') {
+//         gridContent.style.display = 'none';
+//     } else {
+//         gridContent.style.display = 'block';
+//     }
+// });
 
-// CONTACT
-
-const contactHover = document.getElementById('contactHover');
-const contactEmail = document.getElementById('contactEmail');
-
-contactHover.addEventListener('click', () => {
-    if (contactEmail.style.display === 'block') {
-        contactEmail.style.display = 'none';
-    } else {
-        contactEmail.style.display = 'block';
-    }
-});
-
-//WORK
-
-const workOne = document.getElementById('workOne');
-const workOneImg = document.getElementById('workOneImg');
-
-workOne.addEventListener('click', () => {
-    if (workOneImg.style.display === 'block') {
-        workOneImg.style.display = 'none';
-    } else {
-        workOneImg.style.display = 'block';
-    }
-});
-
-const workTwo = document.getElementById('workTwo');
-const workTwoImg = document.getElementById('workTwoImg');
-
-workTwo.addEventListener('click', () => {
-    if (workTwoImg.style.display === 'block') {
-        workTwoImg.style.display = 'none';
-    } else {
-        workTwoImg.style.display = 'block';
-    }
-});
-
-const workThree = document.getElementById('workThree');
-const workThreeImg = document.getElementById('workThreeImg');
-
-workThree.addEventListener('click', () => {
-    if (workThreeImg.style.display === 'block') {
-        workThreeImg.style.display = 'none';
-    } else {
-        workThreeImg.style.display = 'block';
-    }
-});
-
-const workFour = document.getElementById('workFour');
-const workFourImg = document.getElementById('workFourImg');
-
-workFour.addEventListener('click', () => {
-    if (workFourImg.style.display === 'block') {
-        workFourImg.style.display = 'none';
-    } else {
-        workFourImg.style.display = 'block';
-    }
-});
-
-const workFive = document.getElementById('workFive');
-const workFiveImg = document.getElementById('workFiveImg');
-
-workFive.addEventListener('click', () => {
-    if (workFiveImg.style.display === 'block') {
-        workFiveImg.style.display = 'none';
-    } else {
-        workFiveImg.style.display = 'block';
-    }
-});
-
-const workSix = document.getElementById('workSix');
-const workSixImg = document.getElementById('workSixImg');
-
-workSix.addEventListener('click', () => {
-    if (workSixImg.style.display === 'block') {
-        workSixImg.style.display = 'none';
-    } else {
-        workSixImg.style.display = 'block';
-    }
-});
-
-const workSeven = document.getElementById('workSeven');
-const workSevenImg = document.getElementById('workSevenImg');
-
-workSeven.addEventListener('click', () => {
-    if (workSevenImg.style.display === 'block') {
-        workSevenImg.style.display = 'none';
-    } else {
-        workSevenImg.style.display = 'block';
-    }
-});
-
-const posterMisc = document.getElementById('posterMisc');
-const posterMiscImg = document.getElementById('posterMiscImg');
-
-posterMisc.addEventListener('click', () => {
-    if (posterMiscImg.style.display === 'block') {
-        posterMiscImg.style.display = 'none';
-    } else {
-        posterMiscImg.style.display = 'block';
-    }
-});
-
-const workEight = document.getElementById('workEight');
-const workEightImg = document.getElementById('workEightImg');
-
-workEight.addEventListener('click', () => {
-    if (workEightImg.style.display === 'block') {
-        workEightImg.style.display = 'none';
-    } else {
-        workEightImg.style.display = 'block';
-    }
-});
-
-const workNine = document.getElementById('workNine');
-const workNineImg = document.getElementById('workNineImg');
-
-workNine.addEventListener('click', () => {
-    if (workNineImg.style.display === 'block') {
-        workNineImg.style.display = 'none';
-    } else {
-        workNineImg.style.display = 'block';
-    }
-});
 
 
 
